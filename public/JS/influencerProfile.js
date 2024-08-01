87b6111b4942a92d04251aaa39148ef97b672a2a
Akshit Bansal <artistbansal2004@gmail.com>
2024-08-01 20:28:34 +0530

    Update

---
+++ public/JS/influencerProfile.js
@@ -0,0 +1,37 @@
+$(document).ready(function()
+{
+    let active=localStorage.getItem("activeuser");
+    $("#txtEmail").val(active).prop("readonly",true);
+    $(document).ajaxStart(function()
+    {
+        $("#bg").css("display","block");
+        $("#wait").css("display","block");
+    })
+    $(document).ajaxStop(function(){
+        $("#wait").css("display","none");
+        $("#bg").css("display","none");
+    })
+})
+
+$(document).ready(function()
+{
+    if(localStorage.getItem("activeuser")==null)
+    {
+        location.href="index.html";
+        return;
+    }
+})
+
+function doPrev(fileCtrl,imgPrev)
+{
+    let [file] = fileCtrl.files
+    if (file) {
+        imgPrev.src = URL.createObjectURL(file)
+    }
+}
+
+function doLogout()
+{
+    localStorage.removeItem("activeuser");
+    location.href="index.html";
+}
